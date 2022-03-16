67e29967aa1e119e1e49fd883e735e42c49635e1
soham biswas <96686822+PentesterSoham@users.noreply.github.com>
2022-03-16 13:18:01 +0530

    Update auto-rtl8188eus-driver.sh

--- auto-rtl8188eus-driver.sh
+++ auto-rtl8188eus-driver.sh
@@ -3,13 +3,13 @@ echo "check out my Youtube channel :-  https://www.youtube.com/channel/UCdEAvbBJ
 cd /tmp
 sudo apt update
 sudo apt upgrade -y
-sudo apt  install dkms bc wget git
+sudo apt  install dkms bc wget git -y
 sudo git clone https://github.com/aircrack-ng/rtl8188eus.git
 sudo rmmod r8188eu.ko
 cd rtl8188eus
 sudo wget https://raw.githubusercontent.com/PentesterSoham/rtl8188eus_driver/main/rtw_br_ext.c_error --output-document=/tmp/rtl8188eus/core/rtw_br_ext.c
-cd /tmp/rtl8188eus/core
+cd /tmp/rtl8188eus
 sudo make && sudo make install
-sudo echo 'blacklist r8188eu'|sudo tee -a '/etc/modprobe.d/realtek.conf'
+sudo echo 'blacklist r8188eu' > '/etc/modprobe.d/realtek.conf'
 echo "Please reboot the system..."
 echo "100% done"
